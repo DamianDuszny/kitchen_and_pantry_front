1f6866d3e1b015f205b414a068b0fffa50640120
Damian Duszny <damian.duszny@gmail.com>
2024-09-29 20:06:53 +0200

    edit product refactor

--- src/views/AddPantryProduct.tsx
+++ src/views/AddPantryProduct.tsx
@@ -2,9 +2,15 @@ import {useState} from "react";
 import FindProducts from "../components/FindProducts";
 import EditProductForm from "../components/EditProductForm";
 import StockProductList from "../components/StockProductList";
+import {productStock} from "../../domain/dto";
 export default function AddPantryProduct() {
     const [stock, setStock] = useState(null);
-    const [productsStock, setProductsStock] = useState(null);
+    const [productsStock, setProductsStock] = useState<productStock | null>(null);
+    if(productsStock) {
+        productsStock.amount = 0;
+        productsStock.net_weight = 0;
+        productsStock.expiration_date = '';
+    }
     return productsStock ? <>
         <button className={"btn btn-primary mb-1 btn-block"} onClick={() => setProductsStock(null)}>Powrót</button>
         <EditProductForm product={productsStock}/>
